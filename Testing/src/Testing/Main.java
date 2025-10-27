b2eaf274a3d3eb37c48779335db72dc753e84db6
Omer <omershachar98@icloud.com>
2025-10-27 09:11:55 +0200

    added an instance of Hi in Main and used hi() method

--- Testing/src/Testing/Main.java
+++ Testing/src/Testing/Main.java
@@ -3,8 +3,8 @@ package Testing;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("HI");
-		System.out.println("Check!");
+		Hi h = new Hi();
+		h.hi();
 	}
 
 }
